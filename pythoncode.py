20b1afb328ba4c7c88d5661bffee46c179dc4b39
BuvaneshwaranS <79571035+BuvaneshwaranS@users.noreply.github.com>
2021-02-25 19:58:49 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -1,158 +1,91 @@
-n = int(input("Enter the limit:"))
 i = 0
 j = 1
-
-
-def condition(choice):
-    i = 0
-    if (choice == 1):
-        f = input("Enter the Author Name:")
-        if f in dictA.values():
-            print(dictA)
-            i += 1
-        if f in dictB.values():
-            print(dictB)
-            i += 1
-        if f in dictC.values():
-            print(dictC)
-            i += 1
-        if (i == 0):
-            i = 0
-            print("No Books available")
-    if (choice == 2):
-        if choice == 2:
-            a = dictA["Book_cost"]
-            b = dictB["Book_cost"]
-            c = dictC["Book_cost"]
-            if (a > b) and (a > c):
-                print("a1:" + a)
-                if (b > c):
-                    print("b1:" + b)
-                    print(dictC)
-                    print(dictB)
-                    print(dictA)
-                else:
-                    print("c1:" + c)
-                    print(dictB)
-                    print(dictC)
-                    print(dictA)
-            elif (b > a) and (b > c):
-                print("b2:" + b)
-                if (a > c):
-                    print("a2:" + a)
-                    print(dictC)
-                    print(dictA)
-                    print(dictB)
-                else:
-                    print("c2:" + c)
-                    print(dictA)
-                    print(dictC)
-                    print(dictB)
-            elif (c > a) and (c > b):
-                print("c3:" + c)
-                if (a > b):
-                    print("a3:" + a)
-                    print(dictB)
-                    print(dictA)
-                    print(dictC)
-                else:
-                    print("b3:" + b)
-                    print(dictA)
-                    print(dictB)
-                    print(dictC)
-    if (choice == 3):
-        i = 0
-        a = dictA["Pub_year"]
-        b = dictB["Pub_year"]
-        c = dictC["Pub_year"]
-        f = input("Enter The Author Name:")
-        y = input("Enter The Publishing Year:")
-        if f in dictA.values():
-            if y == a:
-                print(dictA)
-                i += 1
-        if f in dictB.values():
-            if y == b:
-                print(dictB)
-                i += 1
-        if f in dictC.values():
-            if y == c:
-                print(dictC)
-                i += 1
-        if i == 0:
-            i = 0
-            print("No Books available")
-    if choice == 4:
-        f = input("Enter the Author Name:")
-        if dictA["Pub_year"] > dictB["Pub_year"]:
-            if dictB["Pub_year"] > dictC["Pub_year"]:
-                if f in dictC.values():
-                    print(dictC)
-                if f in dictB.values():
-                    print(dictB)
-                if f in dictA.values():
-                    print(dictA)
-            elif dictA["Pub_year"] > dictC["Pub_year"]:
-                if f in dictB.values():
-                    print(dictB)
-                if f in dictC.values():
-                    print(dictC)
-                if f in dictA.values():
-                    print(dictA)
-            else:
-                if f in dictB.values():
-                    print(dictB)
-                if f in dictA.values():
-                    print(dictA)
-                if f in dictC.values():
-                    print(dictC)
-
-        elif dictB["Pub_year"] > dictC["Pub_year"]:
-            if dictA["Pub_year"] > dictC["Pub_year"]:
-                if f in dictC.values():
-                    print(dictC)
-                if f in dictA.values():
-                    print(dictA)
-                if f in dictB.values():
-                    print(dictB)
-            elif dictA["Pub_year"] > dictB["Pub_year"]:
-                if f in dictA.values():
-                    print(dictA)
-                if f in dictC.values():
-                    print(dictC)
-                if f in dictB.values():
-                    print(dictB)
-            else:
-                if f in dictA.values():
-                    print(dictA)
-                if f in dictB.values():
-                    print(dictB)
-                if f in dictC.values():
-                    print(dictC)
-
-    if choice == 5:
-        exit()
-
-
-for i in range(0, n):
-    Author_name = input("Enter The Author_name:")
-    Book_name = input("Enter The Book_name:")
-    Book_version = input("Enter The Book_version:")
-    Pub_year = input("Enter The Pub_year:")
-    Book_cost = input("Enter The Book_cost in same digits:")
-    add = int(input("Enter the dict number:"));
-    if (add == 1):
-        dictA = {"Author_name": Author_name, "Book_name": Book_name, "Book_version": Book_version, "Pub_year": Pub_year,
-                 "Book_cost": Book_cost}
-    elif (add == 2):
-        dictB = {"Author_name": Author_name, "Book_name": Book_name, "Book_version": Book_version, "Pub_year": Pub_year,
-                 "Book_cost": Book_cost}
-    elif (add == 3):
-        dictC = {"Author_name": Author_name, "Book_name": Book_name, "Book_version": Book_version, "Pub_year": Pub_year,
-                 "Book_cost": Book_cost}
+k = 1
+input1_string = input('Enter the Author names separated by space: ')
+input2_string = input('Enter the Books name separated by space :')
+input3_string = input('Enter the Books version separated by space:')
+input4_string = input('Enter the Pulished year of the books separated by space: ')
+input5_string = input('Enter the Costs of the Book separated by space:')
+print("\n")
+Author_name = input1_string.split()
+Book_name = input2_string.split()
+Book_version = input3_string.split()
+Published_year = input4_string.split()
+a = input4_string.split()
+Book_cost = input5_string.split()
+b = input5_string.split()
+print(len(Published_year))
 
 print(
-    "1. Enter Author Name\n2. Enter Price\n3. Enter Author Name and Year of Publishing\n4.Enter Author Name,Publishing Year for listing\n5.To Exit")
-while j == 1:
-    choice = int(input("Enter the choice:"))
-    condition(choice)
+    "Enter 1 to Display all the details of application by a given author\nEnter 2 to Sort the details of application in the increasing order of price\nEnter 3 to Display the details of applications published by a given publisher in a given year\nEnter 4 to Sort the list of applications in the increasing order of two fields , author and publishing year of the books\nEnter 5 to Exit")
+while k == 1:
+    choice = int(input('Enter the choice:'))
+    Book_cost = input5_string.split()
+    if choice == 1:
+        j = 0
+        f = input('Enter the author name:')
+        for i in range(0, len(Author_name)):
+            if f == Author_name[i]:
+                print('Author_name:' + Author_name[i] + ',Book_name:' + Book_name[
+                    i] + ',Book_version:' + Book_version[i] + ',Published_year:' +
+                      Published_year[i] + ',Book_cost:' + Book_cost[i])
+                j = 0
+            elif j == 1 and i == len(Author_name) - 1:
+                print("No Books available...")
+
+    if choice == 2:
+        m = 1
+        for i in range(0, len(Book_cost)):
+            Book_cost[i] = int(Book_cost[i])
+            b[i] = int(b[i])
+        for i in range(0, len(Book_cost)):
+            for j in range(i + 1, len(Book_cost)):
+                if b[i] > b[j]:
+                    temp = b[i]
+                    b[i] = b[j]
+                    b[j] = temp
+        for i in range(0, len(Book_cost)):
+            for j in range(0, len(Book_cost)):
+                if b[i] == Book_cost[j]:
+                    print('Author_name:' + Author_name[j] + ',Book_name:' + Book_name[
+                        j] + ',Book_version:' + Book_version[j] + ',Published_year:' +
+                          Published_year[j]+'\nBook_cost:' )
+                    print( Book_cost[j])
+                    m = 0
+                elif m == 1 and i == len(Book_cost) - 1:
+                    print("No Books available...")
+    if choice == 3:
+        m = 1
+        f = input("Enter the Author name:")
+        y = input("Enter the Published year of the book:")
+        for i in range(0, len(Author_name)):
+            if f == Author_name[i]:
+                if y == Published_year[i]:
+                    print('Author_name:' + Author_name[i] + ',Book_name:' + Book_name[
+                        i] + ',Book_version:' + Book_version[i] + ',Published_year:' +
+                          Published_year[i] + ',Book_cost:' + Book_cost[i])
+                    m = 0
+            elif m == 1 and i == len(Author_name):
+                print("No Books available...")
+
+    if choice == 4:
+        m = 1
+        f = input("Enter the Author Name:")
+        for i in range(0, len(Published_year)):
+            for j in range(i + 1, len(Published_year)):
+                if a[i] > a[j]:
+                    temp = a[i]
+                    a[i] = a[j]
+                    a[j] = temp
+        for i in range(0, len(Published_year)):
+            for j in range(0, len(Published_year)):
+                if a[i] == Published_year[j]:
+                    if f == Author_name[j]:
+                        print('Author_name:' + Author_name[j] + ',Book_name:' + Book_name[
+                            j] + ',Book_version:' + Book_version[j] + ',Published_year:' +
+                              Published_year[j] + ',Book_cost:' + Book_cost[j])
+                        m = 0
+                elif m == 1 and i == len(Published_year):
+                    print("No Books available...")
+    if choice == 5:
+        exit()
